27924d7f02442cf9a0591435b782d8fda29a0884
566325 <nethisireesha288@gmail.com>
2023-09-27 12:08:28 +0530

    [Sireesha]:Added PlayerRolles The Die UseCase2

---
+++ PlayerRollsDie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class PlayerRollsDie
+    {
+        public void Start()
+        {
+            int Player_Position = 0;
+            int PlayerOne;
+            PlayerOne = Player_Position;
+            Console.WriteLine("Player One position is " + PlayerOne);
+            PlayerOne = RollsDie();
+            Console.WriteLine("Player Rolles the Die To Show the Position Player Position Is " + PlayerOne) ;
+        }
+        public int RollsDie()
+        {
+            
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            return dice;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //SinglePlayer Game= new SinglePlayer();
+            //Game.Start();
+            PlayerRollsDie Game= new PlayerRollsDie();
+            Game.Start();
+
+
+            //Random random = new Random();
+            //int Count = random.Next(1, 6);
+        }
+
+    }
+}
