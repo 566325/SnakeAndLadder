a7db32efb3db6f2fd4eec9f6d2545e75bbb7b520
566325 <nethisireesha288@gmail.com>
2023-09-27 12:04:17 +0530

    [Sireesha]:Added Game Played With Single Player

---
+++ SinglePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SinglePlayer
+    {
+        public void Start()
+        {
+            int Player_Position = 0;
+            int PlayerOne;
+            PlayerOne = Player_Position;
+            Console.WriteLine("Player One position is " + PlayerOne);
+        }
+
+    }
+}
